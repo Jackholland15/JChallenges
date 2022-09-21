ccf5434318a2d0efbec3a73034443eb627bafb29
Holland <M1704323@middlesbro.ac.uk>
2022-09-21 16:51:44 +0100

    initial commit

---
+++ src/Challenge/Challenge.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenge;
+
+/**
+ *
+ * @author m1704323
+ */
+public class Challenge {
+
+    public static void main(String[] args) {
+        whileLoop();
+    }
+    
+    
+    public static void whileLoop(){
+        int Multi = 5;
+        System.out.println("fjtyju");
+        
+    }
+    
+    public static void example2(){
+        
+    }
+}
